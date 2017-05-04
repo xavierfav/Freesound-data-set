c5e1db6302ace920632121a16dade4794a30869d
xavier <xavier.favory@gmail.com>
2017-05-04 16:37:38 +0200

    several add

--- script_plot2.py
+++ script_plot2.py
@@ -7,13 +7,6 @@ import numpy as np
 # PRINT VOTES
 #votes_TT = pickle.load(open('votes_TT_all.pkl','rb'))
 votes_TT = pickle.load(open('votes_new2.pkl','rb'))
-nb_access_FS_per_category_dict = json.load(open('nb_access_FS_per_category_dict', 'rb'))
-for i in votes_TT:
-    print i[1].ljust(110) + str(i[2]).ljust(10) + str(i[3]).ljust(10)  + str(i[4]).ljust(10)  + str(i[5]).ljust(10) + str(i[6]).ljust(10)+ str(i[7]).ljust(10)# + str(i[8])
-
-for i in votes_TT:
-    print i[1].ljust(110) + str("{0:.2f}".format(i[2]/float(i[6]))).ljust(10) + str("{0:.2f}".format(i[3]/float(i[6]))).ljust(10)  + str("{0:.2f}".format(i[4]/float(i[6]))).ljust(10)  + str("{0:.2f}".format(i[5]/float(i[6]))).ljust(10) + str(i[6]).ljust(10)+ str(i[7]).ljust(10)# + str(i[8])
-
 
 # PLOT P NP
 # index of changes of category
@@ -26,13 +19,12 @@ sns.set(style="whitegrid")
 ax = plt.axes()        
 ax.xaxis.grid()
 x = range(len(votes_TT)) 
-#votes_TT = sorted(votes_TT, key=lambda c: c[2]+c[3])
+votes_TT = sorted(votes_TT, key=lambda c: c[2]+c[3])
 
 v_np = [i[5] for i in votes_TT] # extract non present votes
 v_pp = [i[2] for i in votes_TT] # extract present and predominant votes
 v_pnp = [i[3] for i in votes_TT] # extract present but not predominant votes
 v_u = [i[4] for i in votes_TT] # extract usure votes
-access_FS_order = [nb_access_FS_per_category_dict[i[0]] for i in votes_TT]
 
 #normalized
 #v_np = [i[5]/float(i[6]) for i in votes_TT] # extract non present votes
@@ -51,7 +43,7 @@ for idx, name in enumerate(lines_names):
         plt.axvspan(lines_coord[idx]-0.1, lines_coord_end[idx]+0.9, facecolor='0.5', alpha=0.1)
 
 # limit axis
-plt.xlim(0,398)
+#plt.xlim(0,398)
 plt.ylim(-300,420)
 
 # fix counts axis positive values
@@ -69,40 +61,5 @@ ax.set_ylabel('Vote counts', fontsize=15)
 plt.savefig('votes.pdf', format='pdf')
 plt.show()
 
-
-# PLOT ACCESS FS
-sns.set(style="whitegrid")
-ax = plt.axes()        
-ax.xaxis.grid()
-x = range(len(votes_TT)) 
-p1 = plt.bar(x, access_FS_order, facecolor='#6e6ef6', edgecolor='white')
-# plot the lines delimiting categories groups
-for idx, name in enumerate(lines_names):
-    if idx%2 == 0:
-        plt.axvspan(lines_coord[idx]-0.1, lines_coord_end[idx]+0.9, facecolor='0.5', alpha=0.1)
-# limit axis
-plt.xlim(0,398)
-# add titles
-ax.set_xlabel('Categories')
-ax.set_ylabel('Access FS count')
-plt.title('Super nice plot', fontsize=20)
-plt.show()
-
-
-
-# EX: PRINT NUM xxx PER GROUP CATEGORY
-for idx,_ in enumerate(lines_coord):
-    print sum([i[2] for i in votes_TT[lines_coord[idx]:lines_coord_end[idx]+1]])
-
-print '\n'
-for idx,o in enumerate(lines_coord):
-    print sum([i[4] for i in votes_TT[o:lines_coord_end[idx]]])/float((lines_coord_end[idx]-lines_coord[idx]))
-
-print '\n'
-for idx,o in enumerate(lines_coord):
-    print sum([i for i in access_FS_order[o:lines_coord_end[idx]]])/float(sum([v[6] for v in votes_TT[o:lines_coord_end[idx]]]))
-
-for idx,i in enumerate(votes_TT):                                                
-    print i[1].ljust(110) + str(access_FS_order[idx]/float(i[6]))
 
 
