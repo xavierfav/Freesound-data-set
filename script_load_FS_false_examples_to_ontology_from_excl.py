dfd2b46d76215e05758ed63f1d4f3651060aab39
xavierfav <xavier.favory@gmail.com>
2017-10-17 14:25:16 +0200

    Add script false examples

---
+++ script_load_FS_false_examples_to_ontology_from_excl.py
@@ -0,0 +1,45 @@
+from openpyxl import load_workbook
+import json
+import numpy as np
+
+
+# wb is your workbook
+wb = load_workbook('Freesound FALSE examples for FSD.xlsx')
+
+# list of dictionary oer class, to store the examples and put it into json
+list_examples =[]
+
+# get the content of a sheet
+ws = wb.get_sheet_by_name('Sheet1')
+for row in ws.iter_rows('B{}:B{}'.format(4, ws.max_row)):
+    for cell in row:
+        # if cell contains an example
+        if cell.value != None:
+            cat_id = str(row[0].value)
+            cat_examples = []
+            
+            for idx in range(3):
+                if ws.cell(row=row[0].row, column=4+idx).value  != None:
+                    cat_examples.append(int(ws.cell(row=row[0].row, column=4+idx).value))
+
+            # create dict with 2 keys
+            cat_dict = {}
+            cat_dict['id'] = cat_id
+            cat_dict['negative_examples_FS'] = cat_examples
+
+            # store the dict in a list
+            list_examples.append(cat_dict)
+
+            
+# --------------------------------------------------
+with open('ontology/ontology_preCrowd.json') as fd:
+    onto_data = json.load(fd)
+
+for categ in list_examples:
+    for ii in np.arange(len(onto_data)):
+        if categ['id'] == onto_data[ii]['id']:
+            onto_data[ii]['negative_examples_FS'] = categ['negative_examples_FS']
+            break
+
+#  dump updated list to json
+json.dump(onto_data, open('ontology/ontology_preCrowd_temp.json','w'), indent=4)
