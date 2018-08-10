a8ba2458f437ba84e38e35a8199b0c9656eb286f
eduardo fonseca <eduardo.fonseca.montero@gmail.com>
2018-08-10 13:10:55 +0200

    Add retrieve more metadata and store json for new files only

--- script_retrieve_files_metadata_FSAPI.py
+++ script_retrieve_files_metadata_FSAPI.py
@@ -40,8 +40,8 @@ data_onto_by_id = {o['id']: o for o in data_onto}
 data_onto_by_name = {o['name']: o for o in data_onto}
 
 # NOTE:
-# Aug2: last dump before the addition:
-# - of 30k new FSounds (last year) with original mapping +
+# Aug2: last dump before the addition of:
+# - 30k new FSounds (last year) with original mapping +
 # - 40k (old sounds that were never included in the original mapping)
 # Aug3: dump right after that
 
@@ -101,36 +101,9 @@ def query_freesound_by_id(list_ids):
     for sub_list_ids in chunks(list_ids, 150):
         filter_str = 'id:(' + ' OR '.join([str(i) for i in sub_list_ids]) + ')'
         # page_result = client.text_search(query="", fields="id,pack,pack_name", page_size=50, filter=filter_str)
-        page_result = client.text_search(query="", fields="id,duration", page_size=150, filter=filter_str)
+        page_result = client.text_search(query="", fields="id,duration,license,username,tags,name", page_size=150, filter=filter_str)
         results += [s for s in page_result]
     return results
-
-
-# maybe not needed
-def get_sounds_from_pack(sound_id_target):
-
-    client = freesound.FreesoundClient()
-    client.set_token("eaa4f46407adf86c35c5d5796fd6ea8b05515dca", "token")
-
-    # given a sound_id_target which pack we want to omit, retrieve pack_id
-    sound = client.get_sound(sound_id_target)
-    pack_id = int(sound.pack.split('/')[-2])
-    print pack_id, sound.pack_name
-
-    # retrieve the list of sound ids in the pack
-    pack = client.get_pack(pack_id)
-    pack_sounds = pack.get_sounds(fields="id,name,username", page_size=150)
-
-    list_ids_pack_sounds = [sound.id for sound in pack_sounds]
-    list_ids_pack_sounds.sort()
-
-    # confirm
-    print 'all the %d sounds in the pack: %s, of user %s' % (len(list_ids_pack_sounds), pack.name, pack.username)
-    for idx, sound in enumerate(pack_sounds):
-        print (idx + 1), sound.id, sound.name
-
-    return list_ids_pack_sounds
-
 
 
 """
@@ -138,54 +111,76 @@ def get_sounds_from_pack(sound_id_target):
 """
 
 # create list of ids that are not in data_mapping (because they are new)
-list_ids_lack_duration = []
+list_ids_lack_info = []
 
 # data_mapping.keys() returns unicode fs_ids: convert to int to compare in this format (much faster than unicode)
 current_ids = [int(id) for id in data_mapping.keys()]
 
-# go through candidates of every category: are they in mapping? else to_list
+# go through candidates of every category: are they in data_mapping? else to_list
 for idx, (cat_id, groups) in enumerate(data_votes_raw.iteritems(), 1):
     list_tmp_noduration = [fs_id for fs_id in groups['candidates'] if fs_id not in current_ids]
     print("%d - %-25s: new sounds: %-3d" % (idx, data_onto_by_id[cat_id]['name'], len(list_tmp_noduration)))
-    list_ids_lack_duration += list_tmp_noduration
-    # if idx == 3:
+    list_ids_lack_info += list_tmp_noduration
+    # if idx == 5:
     #     break
 
-print('Number of sounds not found in data_mapping; they must be new from last year (BEFORE SET): %d' % len(list_ids_lack_duration))
+print('Number of candidate annotations not found in data_mapping; they must be new from last year (BEFORE SET): %d' % len(list_ids_lack_info))
 
 # naturally, there can be multiple annotations for every sound
-list_ids_lack_duration_set = list(set(list_ids_lack_duration))
-print('Number of sounds not found in data_mapping; they must be new from last year (AFTER SET): %d' % len(list_ids_lack_duration_set))
+list_ids_lack_info_set = list(set(list_ids_lack_info))
+print('Number of sounds not found in data_mapping; they must be new from last year (AFTER SET): %d' % len(list_ids_lack_info_set))
 
-# now retrieve duration for sounds that do not have it
+
+"""
+***************************************************************************retrieve info for sounds that do not have it
+"""
+
 # instead of making a request for every id, lets optimize it
-list_sound_objects = query_freesound_by_id(list_ids_lack_duration_set)
+list_sound_objects = query_freesound_by_id(list_ids_lack_info_set)
 
-print('Number of sound objects retrieved from FS: %d out of %d' % (len(list_sound_objects), len(list_ids_lack_duration_set)))
+print('\nNumber of sound objects retrieved from FS: %d out of %d' % (len(list_sound_objects), len(list_ids_lack_info_set)))
 
 # any problem retrieving info from FS?
-unaccesible_sound_ids = list(set(list_ids_lack_duration_set) - set([s.id for s in list_sound_objects]))
+unaccesible_sound_ids = list(set(list_ids_lack_info_set) - set([s.id for s in list_sound_objects]))
 if len(unaccesible_sound_ids) > 0:
     print('\nWe could not get info in %d sound(s)' % len(unaccesible_sound_ids))
     print('List of ids without FS info:')
     print(unaccesible_sound_ids)
 
-# finally, update the data_mapping with the new sounds and their durations
-data_mapping_updated = copy.deepcopy(data_mapping)
+# create dict with the new sounds and their metadata
+data_mapping_only30k_1718 = {}
+
 for s in list_sound_objects:
-    data_mapping_updated[str(s.id)] = {}
-    data_mapping_updated[str(s.id)]['duration'] = s.duration
+    data_mapping_only30k_1718[str(s.id)] = {}
+    data_mapping_only30k_1718[str(s.id)]['duration'] = s.duration
+    data_mapping_only30k_1718[str(s.id)]['license'] = s.license
+    data_mapping_only30k_1718[str(s.id)]['username'] = s.username
+    data_mapping_only30k_1718[str(s.id)]['tags'] = s.tags
+    data_mapping_only30k_1718[str(s.id)]['name'] = s.name
 
-# save it in FS_sounds_ASO_postIQA_Aug_08.json, to be loaded from script_current_state_FSD.py
-json.dump(data_mapping_updated, open(FOLDER_DATA + '/json/FS_sounds_ASO_postIQA_Aug_08.json', 'w'))
 
+if len(unaccesible_sound_ids) > 0:
+    # dos sonidos deleted from Freesound: Fred had the info
+    data_mapping_only30k_1718[str(434155)] = {}
+    data_mapping_only30k_1718[str(434155)]['duration'] = 17.18
+    data_mapping_only30k_1718[str(434155)]['license'] = 'unknown'
 
+    data_mapping_only30k_1718[str(434156)] = {}
+    data_mapping_only30k_1718[str(434156)]['duration'] = 18.57
+    data_mapping_only30k_1718[str(434156)]['license'] = 'unknown'
 
+# save the dict with the new 30k sounds from March17-July18
+json.dump(data_mapping_only30k_1718, open(FOLDER_DATA + '/json/FS_sounds_ASO_postIQA_only30k_1718.json', 'w'))
 
-# Number of sounds not found in data_mapping; they must be new from last year (BEFORE SET): 85953
-# Number of sounds not found in data_mapping; they must be new from last year (AFTER SET): 29030
-# Number of sound objects retrieved from FS: 29028 out of 29030
+# Finnaly, merge with old data_mapping and save it in
+# FS_sounds_ASO_postIQA_2018_Aug_08.json, to be loaded from script_current_state_FSD.py
+data_mapping_updated = copy.deepcopy(data_mapping)
+data_mapping_updated.update(data_mapping_only30k_1718)
+json.dump(data_mapping_updated, open(FOLDER_DATA + '/json/FS_sounds_ASO_postIQA_2018_Aug_08.json', 'w'))
+# 297291 files
 
+if len(data_mapping_updated) != 297291:
+    print("Error creating data_mapping_updated")
 
 # NOTES:
 # Number of sound objects retrieved from FS: 29028 out of 29030
@@ -194,11 +189,7 @@ json.dump(data_mapping_updated, open(FOLDER_DATA + '/json/FS_sounds_ASO_postIQA_
 # en total: 297,291
 # en web: 297291
 
-# dos sonidos de los que no se la duracion: Fred la tenia
-data_mapping_updated[str(434155)] = {}
-data_mapping_updated[str(434155)]['duration'] = 17.18
-
-data_mapping_updated[str(434156)] = {}
-data_mapping_updated[str(434156)]['duration'] = 18.57
+# Number of candidate annotations not found in data_mapping; they must be new from last year (BEFORE SET): 85953
+# Number of sounds not found in data_mapping; they must be new from last year (AFTER SET): 29030
 
 a=9
